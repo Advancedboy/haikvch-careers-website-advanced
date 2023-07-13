2a122949f5a00533f830d46491b60606047efaab
Haikevich <51019643+Advancedboy@users.noreply.github.com>
2023-07-13 17:30:45 +0300

    Added list of jobs.

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Minsk, Belarus',
+        'salary': '$12,000'
+    },
+    {
+        'id': 2,
+        'title': 'Marketer',
+        'location': 'Molodechno, Belarus',
+        'salary': '$1000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend',
+        'location': 'remote',
+        'salary': '$10,000'
+    },
+    {
+        'id': 4,
+        'title': 'Low-level software engineer',
+        'location': 'NYC, United States',
+        'salary': '$24,000'
+    }
+]
+
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Haikvch')
 
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == '__main__':
-    print("I'm inside the if now")
     app.run(debug=True)
